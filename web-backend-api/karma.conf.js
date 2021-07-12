20f3746de72948e143e149d6eba9a16b5ed80d91
jackson.werka <jackson.werka@totvs.com.br>
2021-07-12 00:23:25 -0300

    Fix: Ajustes relatório coverage

--- web-backend-api/karma.conf.js
+++ web-backend-api/karma.conf.js
@@ -5,19 +5,24 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-coverage'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './../coverage/web-backend-api'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'src/**/*.ts': ['coverage']
     },
-    reporters: ['progress', 'kjhtml'],
+    coverageReporter: {
+      type : 'html',
+      dir : require('path').join(__dirname, './../coverage/web-backend-api')
+    },
+    reporters: ['progress', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
